de6ff39f9935c788d7f10ff7dc1d40ac0607715e
Shantika <your.email@example.com>
2026-03-10 22:43:04 +0530

    fix drag drop dependency

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,4 +1,4 @@
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 import Note from "./Note";
 
 function NotesList({ notes, deleteNote, editNote, togglePin, setNotes }) {
